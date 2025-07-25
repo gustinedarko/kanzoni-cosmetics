bfbc5222e8bc2446b961dc2df7db25b827bfefc8
gustinedarko <gustine369@gmail.com>
2025-07-25 01:22:38 +0000

    Home page updated

--- src/components/AlertBar.jsx
+++ src/components/AlertBar.jsx
@@ -2,7 +2,7 @@ export default function AlertBar() {
     return (
         <>
         <div className="w-full h-12 justify-items-center bg-[#A7B97E]">
-            <h1 className="leading-none text-center px-2 py-1.5 md:py-3 font-medium">Your skin and hair deserve the best — new deals coming soon!</h1>
+            <h1 className="leading-none text-[#F7F4ED] text-center px-2 py-1.5 md:py-3 font-bold">Your skin and hair deserve the best — new deals coming soon!</h1>
         </div>
         </>
     );
--- src/pages/ProductsHome.jsx
+++ src/pages/ProductsHome.jsx
@@ -5,6 +5,10 @@ import Footer from "../components/Footer";
 import img1 from "../assets/images/istockphoto-A.jpg";
 import img2 from "../assets/images/kaeme-unsplash.jpg";
 import img3 from "../assets/images/istockphoto-B.jpg";
+import cropCream from "../assets/images/cream-crop.jpeg"
+import cropLotion from "../assets/images/lotion-crop.jpeg"
+import cropShowergel from "../assets/images/shower-gel-crop.jpeg"
+import cropSoap from "../assets/images/black-soap-crop.jpeg"
 
 const images = [img1, img2, img3];
 
@@ -32,7 +36,7 @@ export default function ProductsHome() {
                         key={index}
                         src={img}
                         alt={`slide-${index}`}
-                        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out ${index === currentIndex ? "opacity-100 z-10" : "opacity-0 z-0"
+                        className={`absolute top-0 left-0 w-full h-full  object-cover transition-opacity duration-1000 ease-in-out ${index === currentIndex ? "opacity-100 z-10" : "opacity-0 z-0"
                             }`}
                     />
                 ))}
@@ -43,7 +47,64 @@ export default function ProductsHome() {
                 </div>
             </section>
 
-            <section className="w-full h-screen bg-white"></section>
+            <section className="w-full h-screen bg-[#F7F4ED] flex items-center justify-center">
+                <div className="w-2/3">
+                    <h2 className="flex justify-center">Kanzoni</h2>
+                    <h3 className="flex justify-center">Represents ........</h3>
+                    <p className="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, explicabo soluta. Id vel, fugit assumenda cupiditate quae saepe beatae totam velit voluptatem omnis impedit earum magnam iure accusantium tempora consectetur qui cumque praesentium maxime, enim, ab aliquam? Ducimus sequi facere voluptas praesentium quibusdam a quod eligendi pariatur reiciendis perspiciatis. Doloremque?</p>
+                    <span className="flex justify-center"><button>Our story</button></span>
+                    <p className="flex justify-center">Products</p>
+                </div>
+            </section>
+            <section
+                style={{
+                    backgroundImage: `url(${cropCream})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'right',
+                    backgroundAttachment: 'fixed',
+                }}
+                className="w-full h-full md:h-screen">
+                <div className="w-full h-screen flex items-start justify-center pt-[90%] sm:pt-[70%] md:pt-[40%] lg:pt-[35%]">
+                    <button className="bg-white text-black px-6 py-3 rounded shadow-lg">Shop Now</button>
+                </div>
+            </section>
+            <section
+                style={{
+                    backgroundImage: `url(${cropLotion})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'right',
+                    backgroundAttachment: 'fixed',
+                }}
+                className="w-full h-full md:h-screen">
+                <div className="w-full h-screen flex items-start justify-center pt-[90%] sm:pt-[70%] md:pt-[40%] lg:pt-[35%]">
+                    <button className="bg-white text-black px-6 py-3 rounded shadow-lg">Shop Now</button>
+                </div>
+            </section>
+            <section
+                style={{
+                    backgroundImage: `url(${cropShowergel})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'right',
+                    backgroundAttachment: 'fixed',
+                }}
+                className="w-full h-full md:h-screen">
+                <div className="w-full h-screen flex items-start justify-center pt-[90%] sm:pt-[70%] md:pt-[40%] lg:pt-[35%]">
+                    <button className="bg-white text-black px-6 py-3 rounded shadow-lg">Shop Now</button>
+                </div>
+            </section>
+            <section
+                style={{
+                    backgroundImage: `url(${cropSoap})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'right',
+                    backgroundAttachment: 'fixed',
+                }}
+                className="w-full h-full md:h-screen">
+                <div className="w-full h-screen flex items-start justify-center pt-[90%] sm:pt-[70%] md:pt-[40%] lg:pt-[35%]">
+                    <button className="bg-white text-black px-6 py-3 rounded shadow-lg">Shop Now</button>
+                </div>
+            </section>
+            <section className="w-full h-screen"></section>
             <Footer />
         </>
     );
